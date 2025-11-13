9ca0ee3f78b38dc8597841d7ff916d342bc280dd
klesleysantana <klesleysantana@gmail.com>
2025-11-12 23:06:41 -0300

    feat: finalize functional CLI version of password generator and add readme

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.IO;
 using System.Text;
+using System.Security.Cryptography;
+
 
 namespace PasswordGenerator
 {
@@ -40,20 +42,29 @@ namespace PasswordGenerator
             bool chooseNumbers = DataEntries("[Numbers]");
             bool chooseSpecialCharacters = DataEntries("[Special Characters]");
             bool chooseUppercaseLetters = DataEntries("[Uppercase Letters]");
-            
+
             Console.Write($"Enter the length of your password (8 - 128): ");
             int passwordLength;
             while (!int.TryParse(Console.ReadLine(), out passwordLength) || (passwordLength <= 8 && passwordLength >= 128))
             {
                 Console.Write("Erro! Digit a valid value: ");
             }
-            
+
             StringBuilder pool = new StringBuilder();
             pool.Append(PasswordSets.Lowercase);
-
             if (chooseNumbers) pool.Append(PasswordSets.Number);
             if (chooseSpecialCharacters) pool.Append(PasswordSets.Special);
-            if (chooseUppercaseLetters) pool.Append(PasswordSets.Uppercase);          
+            if (chooseUppercaseLetters) pool.Append(PasswordSets.Uppercase);
+
+            StringBuilder password = new StringBuilder(); ;
+            for (int i = 0; i <= passwordLength; i++)
+            {
+                int index = RandomNumberGenerator.GetInt32(pool.Length);
+                password.Append(pool[index]);
+            }
+
+            string finalPassword = password.ToString();
+            Console.WriteLine($"\nGenerated password: {finalPassword}");
 
         }
     }
